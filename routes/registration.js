8b6ea56d20380082a795b7688d2c2ecc2d3c33b0
Grigoriy <kovarscky.g@yandex.ru>
2019-11-25 01:10:21 +0300

    weekend commit

--- routes/registration.js
+++ routes/registration.js
@@ -5,6 +5,7 @@ var lib = require('../lib/lib');
 router.post('/', function(req, res, next) {
     var data = req.body;
     data.service_translation = lib.helper_service_translations(data);
+    data.time_labels = lib.helper_service_duration_label(lib.getCurrenWeek().time_list);
     console.log(data);
     lib.customerRegistration(data, function(result) {
         if (result !== 'slot_busy') {
